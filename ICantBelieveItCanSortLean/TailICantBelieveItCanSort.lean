d87363c29eddd7cfecf15bcb4652d1a4c3849d86
rikitoro <>
2024-12-31 18:41:13 +0900

    minor fix

--- ICantBelieveItCanSortLean/TailICantBelieveItCanSort.lean
+++ ICantBelieveItCanSortLean/TailICantBelieveItCanSort.lean
@@ -33,6 +33,6 @@ def Tail.iCantBelieveItCanSort {α : Type} [Ord α] (arr : Array α) : Array α 
   loop_i arr 0
 
 #eval Tail.iCantBelieveItCanSort #[3,1,4,1,5,9,2,6,5,3]
-
+-- #[1, 1, 2, 3, 3, 4, 5, 5, 6, 9]
 
 end
