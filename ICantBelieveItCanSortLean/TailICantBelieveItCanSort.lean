52b1e3bc569850a4a8d536cbb5203d43599b9bea
rikitoro <>
2024-12-31 18:35:44 +0900

    minor updates

--- ICantBelieveItCanSortLean/TailICantBelieveItCanSort.lean
+++ ICantBelieveItCanSortLean/TailICantBelieveItCanSort.lean
@@ -18,16 +18,17 @@ def Tail.iCantBelieveItCanSort {α : Type} [Ord α] (arr : Array α) : Array α 
       loop_i (loop_j arr i 0 inBounds_i) (i + 1) -- 内側のループ loop_j に i < arr.size の証明を渡す
     else
       arr
-  termination_by arr.size - i
-  decreasing_by
-    have loop_j_size_eq (arr : Array α) (i : Nat) (j : Nat) (inBounds_i : i < arr.size)
-      : (iCantBelieveItCanSort.loop_i.loop_j arr i j inBounds_i).size = arr.size := by
+
+    termination_by arr.size - i
+    decreasing_by
       -- Functional induction を用いて証明
       -- https://lean-lang.org/blog/2024-5-17-functional-induction/
-      induction arr, i, j, inBounds_i using iCantBelieveItCanSort.loop_i.loop_j.induct
-      <;> unfold iCantBelieveItCanSort.loop_i.loop_j <;> simp [*]
-    rw [loop_j_size_eq]
-    omega
+      have loop_j_size_eq (arr : Array α) (i : Nat) (j : Nat) (inBounds_i : i < arr.size)
+        : (iCantBelieveItCanSort.loop_i.loop_j arr i j inBounds_i).size = arr.size := by
+        induction arr, i, j, inBounds_i using iCantBelieveItCanSort.loop_i.loop_j.induct
+        <;> (unfold iCantBelieveItCanSort.loop_i.loop_j ; simp [*])
+      rw [loop_j_size_eq]
+      exact Nat.sub_succ_lt_self arr.size i inBounds_i
 
   loop_i arr 0
 
